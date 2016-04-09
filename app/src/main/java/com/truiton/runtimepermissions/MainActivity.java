28e64e3be66475c58084f1ecdacc63b2ff9ad96a
Mohit Gupt <mohitgupt@gmail.com>
2016-04-10 01:41:25 +0530

    v1 fully functional.

---
+++ app/src/main/java/com/truiton/runtimepermissions/MainActivity.java
@@ -0,0 +1,38 @@
+package com.truiton.runtimepermissions;
+
+import android.Manifest;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends RuntimePermissionsActivity {
+
+    private static final int REQUEST_PERMISSIONS = 20;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                MainActivity.super.requestAppPermissions(new
+                                String[]{Manifest.permission.READ_CONTACTS,
+                                Manifest.permission.WRITE_EXTERNAL_STORAGE}, R.string
+                                .runtime_permissions_txt
+                        , REQUEST_PERMISSIONS);
+            }
+        });
+    }
+
+    @Override
+    public void onPermissionsGranted(final int requestCode) {
+        Toast.makeText(this, "Permissions Received.", Toast.LENGTH_LONG).show();
+    }
+}
